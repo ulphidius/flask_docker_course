0aafa0b0e50104f414a9e0f668eac0d8d9e16b59
LAURENT Louis <louis_laurent@aol.fr>
2021-03-25 11:32:48 +0100

    fix list of commands

--- commands_list.sh
+++ commands_list.sh
@@ -1,6 +1,6 @@
 # Build app images
-docker build --tag flask_docker_class --tag flask_docker_class:1.0 --file Dockerfile.prod 
-docker build --tag flask_docker_class_prod --tag flask_docker_class_prod:1.0 --file Dockerfile.prod
+docker build --tag flask_docker_class --tag flask_docker_class:1.0 --file Dockerfile .
+docker build --tag flask_docker_class_prod --tag flask_docker_class_prod:1.0 --file Dockerfile.prod .
 
 # Create network
 docker network create --driver=bridge docker_course
@@ -22,18 +22,18 @@ curl http://docker_course_app/
 curl http://docker_course_app_prod/
 
 # Start postgresql without volume
-docker run --rm --detach --network bridge --publish 5432:5432 --name postgresql --env POSTGRES_PASSWORD=flask --env POSTGRES_USER=flask --env POSTGRES_DB=course postgres
+docker run --rm --detach --network docker_course --publish 5432:5432 --name postgresql --env POSTGRES_PASSWORD=flask --env POSTGRES_USER=flask --env POSTGRES_DB=course postgres
 psql --user flask --dbname course --host 172.17.0.2 < postgresql/data.sql
 psql --user flask --dbname course --host 172.17.0.2 --command "SELECT * FROM public.user;"
 docker stop postgresql
 
 # Create volume for postgresql data
 docker volume create docker_course
-docker run --rm --detach --network bridge --publish 5432:5432 --volume docker_course:/var/lib/postgresql/data --name postgresql --env POSTGRES_PASSWORD=flask --env POSTGRES_USER=flask --env POSTGRES_DB=course postgres
+docker run --rm --detach --network docker_course --publish 5432:5432 --volume docker_course:/var/lib/postgresql/data --name postgresql --env POSTGRES_PASSWORD=flask --env POSTGRES_USER=flask --env POSTGRES_DB=course postgres
 psql --user flask --dbname course --host 172.17.0.2 < postgresql/data.sql
 psql --user flask --dbname course --host 172.17.0.2 --command "SELECT * FROM public.user;"
 docker stop postgresql
-docker run --rm --detach --network bridge --publish 5432:5432 --volume docker_course:/var/lib/postgresql/data --name postgresql --env POSTGRES_PASSWORD=flask --env POSTGRES_USER=flask --env POSTGRES_DB=course postgres
+docker run --rm --detach --network docker_course --publish 5432:5432 --volume docker_course:/var/lib/postgresql/data --name postgresql --env POSTGRES_PASSWORD=flask --env POSTGRES_USER=flask --env POSTGRES_DB=course postgres
 psql --user flask --dbname course --host 172.17.0.2 --command "SELECT * FROM public.user;"
 docker stop postgresql
 
