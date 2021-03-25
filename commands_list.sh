5d4e86fd3cf2da1922bd4482e2d3848bb120c090
LAURENT Louis <louis_laurent@aol.fr>
2021-03-25 12:10:54 +0100

    add database interaction modify commands for v2.0.0

--- commands_list.sh
+++ commands_list.sh
@@ -1,3 +1,5 @@
+# V1.0.0
+
 # Build app images
 docker build --tag flask_docker_class --tag flask_docker_class:1.0 --file Dockerfile .
 docker build --tag flask_docker_class_prod --tag flask_docker_class_prod:1.0 --file Dockerfile.prod .
@@ -40,4 +42,27 @@ docker stop postgresql
 # Clean up
 docker stop docker_course_app
 docker stop docker_course_app_prod
+docker system prune
+
+# V2.0.0
+
+# Build app images
+docker build --tag flask_docker_class --tag flask_docker_class:1.0 --file Dockerfile .
+docker build --tag flask_docker_class_prod --tag flask_docker_class_prod:1.0 --file Dockerfile.prod .
+
+# Create network
+docker network create --driver=bridge docker_course
+
+# Create volume for postgresql data
+docker volume create docker_course
+docker run --rm --detach --network docker_course --publish 5432:5432 --volume docker_course:/var/lib/postgresql/data --name postgresql --env POSTGRES_PASSWORD=flask --env POSTGRES_USER=flask --env POSTGRES_DB=course postgres
+
+# Run app images
+docker run --rm --interactive --tty --detach --name docker_course_app --network docker_course --env FLASK_IP=0.0.0.0 --env FLASK_PORT=5000 --env DB_NAME=course --env DB_HOST=postgresql --env DB_USER=flask --env DB_PASSWORD=flask --publish 5000:5000 flask_docker_class:latest
+docker run --rm --interactive --tty --detach --name docker_course_app_prod --network docker_course --env FLASK_IP=0.0.0.0 --env FLASK_PORT=5000 --env DB_NAME=course --env DB_HOST=postgresql --env DB_USER=flask --env DB_PASSWORD=flask --publish 5000:5000 flask_docker_class_prod:latest
+
+# Clean up
+docker stop docker_course_app
+docker stop docker_course_app_prod
+docker stop postgresql
 docker system prune
--- flask_docker_course/main.py
+++ flask_docker_course/main.py
@@ -2,9 +2,19 @@ from flask import Flask, jsonify, abort
 from werkzeug.middleware.proxy_fix import ProxyFix
 from .user import User
 from os import getenv
+import psycopg2
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
+database_connection = psycopg2.connect("dbname={} user={} password={} host={}".format(
+    getenv('DB_NAME'),
+    getenv('DB_USER'),
+    getenv('DB_PASSWORD'),
+    getenv('DB_HOST')
+))
+
+cursor = database_connection.cursor()
 
 USERS = [User(1, 'sample_username1', 30), User(2, 'sample_username2', 31)]
 
@@ -12,22 +22,39 @@ USERS = [User(1, 'sample_username1', 30), User(2, 'sample_username2', 31)]
 def not_found(error):
     return jsonify(error=str(error)), 404
 
+@app.errorhandler(500)
+def not_found(error):
+    return jsonify(error=str(error)), 500
+
 @app.route('/', methods=['GET'])
 def root_path():
     return jsonify("Welcome in the root !")
 
 @app.route('/user', methods=['GET'])
 def get_user():
-    return jsonify([user.as_dict() for user in USERS])
+    try:
+        cursor.execute('SELECT id, name, age FROM public.user')
+        rows = cursor.fetchall()
+        users = []
+        for row in rows:
+            users.append(User(row[0], row[1], row[2]))
+        return jsonify([user.as_dict() for user in users])
+
+    except Exception as error:
+        abort(500)
 
 @app.route('/user/<int:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
-    user_to_return = list(filter(lambda user: user._id == user_id, USERS))
+    try:
+        cursor.execute('SELECT id, name, age FROM public.user WHERE id=(%s)', (user_id,))
+        row = cursor.fetchone()
+    except Exception as error:
+        abort(500, error)
     
-    if not user_to_return:
+    if not row:
         abort(404)
-    
-    return jsonify(user_to_return[0].as_dict())
+        
+    return jsonify(User(row[0], row[1], row[2]).as_dict())
 
 def main():
     app.run(host=getenv('FLASK_IP'), port=getenv('FLASK_PORT'))
